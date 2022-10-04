71c2add71c89c5aadbc3a1d3ca89ae45b8969f01
Javier Salazar <javieralejandrosalazartorres@gmail.com>
2022-10-04 10:55:15 -0500

    Primera vista de la aplicación movil

---
+++ constants/products.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    title: "Escanear imagen",
+    image: "https://i.blogs.es/bf0cc5/escanerapps/1366_2000.jpg",
+    price: "Escanear",
+    horizontal: true,
+  },
+  {
+    title: "Realizar una operación matemática",
+    image:
+      "http://3.bp.blogspot.com/-E6raCgF1Jes/VltpIZQFU2I/AAAAAAAAAK4/qbgz3Kz-VRY/s1600/operacioens%2Bmate.gif",
+    price: "Escanear",
+  },
+  {
+    title: "Escanear lugar historico",
+    image:
+      "https://proyectodemocracia.com/wp-content/uploads/2019/03/Catedral-Bas%C3%ADlica-Menor-de-Durango.jpg",
+    price: "Escanear",
+  },
+  {
+    title: "Botánica",
+    image: "https://cdn.techtitute.com/portal-blog/Nomenclatura-botanica.jpg",
+    price: "Escanear",
+    horizontal: true,
+  },
+  {
+    title: "Revisar ortografía",
+    image:
+      "https://www.leonhunter.com/wp-content/uploads/2018/02/84586380_ml.jpg",
+    price: "Escanear",
+  },
+];
