543821eacb9817ae6e7ec459f6c64910b35c2bf4
MrAxelMG <axel_3041190034@utd.edu.mx>
2022-10-27 19:06:48 -0500

    ES-10 V 1.0

--- educaverse/resources/views/welcome.blade.php
+++ educaverse/resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Hola amigos de JIRA
+                                    Hola amigos de JIRA 2 
                                 </div>
                             </div>
                         </div>
