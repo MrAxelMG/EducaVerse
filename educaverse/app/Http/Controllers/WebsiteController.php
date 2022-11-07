ec330d308642e3061efe2b60caebea3a762d63d0
Javier Salazar <javieralejandrosalazartorres@gmail.com>
2022-11-06 20:18:08 -0600

    V 2.3.1

--- educaverse/app/Http/Controllers/WebsiteController.php
+++ educaverse/app/Http/Controllers/WebsiteController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Videojuegos;
+use App\Models\Videojuego;
 
 class WebsiteController extends Controller
 {
 
     public function index()
     {
-        $videojuegos = Videojuegos::join('materias', 'materias.id', '=', 'videojuegos.materia_id')
+        $videojuegos = Videojuego::join('materias', 'materias.id', '=', 'videojuegos.materia_id')
                         ->join('categorias', 'categorias.id', '=', 'videojuegos.categoria_id')
                         ->select('videojuegos.id', 'videojuegos.nombre', 'videojuegos.plataformas', 'videojuegos.jugadores', 'materias.nombre AS nombreMateria', 'categorias.nombre AS nombreCategoria', 'videojuegos.imagen', 'videojuegos.imagen2')
                         ->get();
