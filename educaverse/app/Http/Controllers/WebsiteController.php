4d577434791a238d8fce365d196f034b9e2d341a
JavierSalazarUp <javiersalazar@uptradingexperts.com>
2022-12-06 00:55:39 -0600

    V 4.7.3

--- educaverse/app/Http/Controllers/WebsiteController.php
+++ educaverse/app/Http/Controllers/WebsiteController.php
@@ -4,29 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Videojuego;
-use Monolog\Logger;
-use Monolog\Formatter\LineFormatter;
-use Monolog\Handler\StreamHandler;
-
 
 class WebsiteController extends Controller
 {
 
     public function index()
     {
-            
-        // set the format
-        $output = "%message%";
-        $formatter = new LineFormatter($output);
-        // create a log channel to STDOUT
-        $log = new Logger('my_logger');
-        $streamHandler = new StreamHandler('php://stdout', Logger::WARNING);
-        $streamHandler->setFormatter($formatter);
-        $log->pushHandler($streamHandler);
-        // test messages
-        $log->addWarning("testing 1");
-        $log->addWarning("testing 2");
-
         $videojuegos = Videojuego::join('materias', 'materias.id', '=', 'videojuegos.materia_id')
                         ->join('categorias', 'categorias.id', '=', 'videojuegos.categoria_id')
                         ->select('videojuegos.id', 'videojuegos.nombre', 'videojuegos.plataformas', 'videojuegos.jugadores', 'materias.nombre AS nombreMateria', 'categorias.nombre AS nombreCategoria', 'videojuegos.imagen', 'videojuegos.imagen2', 'videojuegos.descripcion', 'videojuegos.precio')
