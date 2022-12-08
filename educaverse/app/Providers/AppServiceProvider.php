915c88515936f5a6c568dde5a54157fe86be9efc
JavierSalazarUp <javiersalazar@uptradingexperts.com>
2022-12-07 18:22:08 -0600

    v 6.0.4

--- educaverse/app/Providers/AppServiceProvider.php
+++ educaverse/app/Providers/AppServiceProvider.php
@@ -29,8 +29,6 @@ class AppServiceProvider extends ServiceProvider
 });
 
 date_default_timezone_set('America/Mexico_City');
-\Carbon\Carbon::setUTF8(true);
-\Carbon\Carbon::setLocale(config('app.locale'));
-setlocale(LC_ALL, 'es_MX', 'es', 'ES', 'es_MX.utf8');
+setlocale(LC_ALL, 'es_ES.UTF-8');
 }
 }
